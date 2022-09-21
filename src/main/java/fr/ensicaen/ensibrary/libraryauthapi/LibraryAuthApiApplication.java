8085dd01dfe31a13dd5a5aa8bfd6f8bc047c8c4b
Victor Brionne <victor.brionne@gmail.com>
2022-09-21 13:44:38 +0200

    Initial commit

---
+++ src/main/java/fr/ensicaen/ensibrary/libraryauthapi/LibraryAuthApiApplication.java
@@ -0,0 +1,13 @@
+package fr.ensicaen.ensibrary.libraryauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ensicaen/ensibrary/libraryauthapi/LibraryAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensicaen.ensibrary.libraryauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
