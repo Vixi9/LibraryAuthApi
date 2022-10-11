9024caad49eaa94d2e7f5ba35d98d33fa2e1bde0
brionnevi <victor.brionne@gmail.com>
2022-10-11 21:29:11 +0200

    Deleted auth configuration

--- src/main/java/fr/ensicaen/ensibrary/libraryauthapi/configuration/DefaultSecurityConfig.java
+++ src/main/java/fr/ensicaen/ensibrary/libraryauthapi/configuration/DefaultSecurityConfig.java
@@ -4,13 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -18,15 +14,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 )
 @EnableWebSecurity
 public class DefaultSecurityConfig extends GlobalMethodSecurityConfiguration {
-
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated()
-                )
-                .formLogin(withDefaults()).cors().and().csrf().disable();
-        return http.build();
-    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
