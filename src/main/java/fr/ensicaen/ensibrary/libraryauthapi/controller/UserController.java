66b8a91f4e2b4db8227122b1c8923dfadd73accc
brionnevi <victor.brionne@gmail.com>
2022-10-11 22:06:07 +0200

    Corrected config

--- src/main/java/fr/ensicaen/ensibrary/libraryauthapi/controller/UserController.java
+++ src/main/java/fr/ensicaen/ensibrary/libraryauthapi/controller/UserController.java
@@ -16,7 +16,6 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping(value = "user")
-@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserService userService;
